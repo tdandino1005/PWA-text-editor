d32b151247ffaf1924aa9aa87fb3e692a5ed8384
Tony Dandino <tdandino1005@gmail.com>
2023-04-04 22:27:46 -0500

    Added methods that gets content and items and adds it into database.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+console.log('PUT DB');
+const jatDB = await openDB("jate", 1);
+const tx = jatDB.transaction("jate", "readwrite");
+const store = tx.objectStore("jate");
+const request = store.put({jate:content});
+const result = await request;
+console.log("Data saved to database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Added a method that gets all the content from the database
+export const getDb = async () => {
+console.log('GET DB');
+const jatDB = await openDB("jate", 1);
+const tx = jatDB.transaction("jate", "readonly");
+const store = tx.objectStore("jate");
+const request = store.getAll();
+const result = await request;
+console.log(result);
+};
+
 
 initdb();
