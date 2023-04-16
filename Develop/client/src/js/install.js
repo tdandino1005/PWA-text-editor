ad56e51f3873b5ccfd603b32a8253f49dd763a54
Tony Dandino <tdandino1005@gmail.com>
2023-04-16 13:44:12 -0500

    Added clear prompts install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,30 +1,31 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+
+  // Remove the 'hidden' class from the install button container
+  butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-        return;
-    }
-    promptEvent.prompt();
-    const result = await promptEvent.userChoice;
-    if (result.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-    } else {
-        console.log('User dismissed the A2HS prompt');
-    }
-    window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+    // Show the install prompt
+  promptEvent.prompt();
+
+    // Wait for the user to respond to the prompt
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+window.addEventListener("appinstalled", (event) => {
+// Log install to analytics
+  window.deferredPrompt = null;
 });
